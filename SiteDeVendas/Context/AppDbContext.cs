9daba715aa033055089410d7c86c40ed172d8320
DanilloAlvesSouza <danillo.alves.souza@outlook.com>
2023-09-05 16:15:13 -0300

    Implementando EF Core 7 + String de conexão com BD + segmentando program/startup

---
+++ SiteDeVendas/Context/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using KontrolaPoc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KontrolaPoc.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Chamado> Chamados { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Equipamento> Equipamentos { get; set; }
+        public DbSet<Filial> Filiais { get; set; }
+        public DbSet<Gravidade> Gravidades { get; set; }
+        public DbSet<ItemChamado> ItemChamados { get; set; }
+        public DbSet<Modalidade> Modalidades { get; set; }
+        public DbSet<Status> Status { get; set; }
+        public DbSet<Tendencia> Tendencias { get; set; }
+        public DbSet<Urgencia> Urgencias { get; set; }
+
+
+
+    }
+}

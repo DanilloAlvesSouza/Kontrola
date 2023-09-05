8ee984cbe005120fe82b04bb6295d65e224d50cb
DanilloAlvesSouza <danillo.alves.souza@outlook.com>
2023-09-05 20:00:19 -0300

    Aplicando Migrations

---
+++ SiteDeVendas/Migrations/20230905224843_MigracaoInicial.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KontrolaPoc.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracaoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Gravidades",
+                columns: table => new
+                {
+                    GravidadeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gravidades", x => x.GravidadeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lanches",
+                columns: table => new
+                {
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cnpj = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lanches", x => x.ClienteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Modalidades",
+                columns: table => new
+                {
+                    ModalidadeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modalidades", x => x.ModalidadeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Status",
+                columns: table => new
+                {
+                    StatusId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Status", x => x.StatusId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tendencias",
+                columns: table => new
+                {
+                    TendenciaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tendencias", x => x.TendenciaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Urgencias",
+                columns: table => new
+                {
+                    UrgenciaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Urgencias", x => x.UrgenciaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Filiais",
+                columns: table => new
+                {
+                    FilialId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filiais", x => x.FilialId);
+                    table.ForeignKey(
+                        name: "FK_Filiais_Lanches_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Lanches",
+                        principalColumn: "ClienteId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Chamados",
+                columns: table => new
+                {
+                    ChamadoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DataInicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataFechamento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Diagnostico = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Pendencia = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Conclusao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StatusId = table.Column<int>(type: "int", nullable: false),
+                    ModalidadeId = table.Column<int>(type: "int", nullable: false),
+                    GravidadeId = table.Column<int>(type: "int", nullable: false),
+                    UrgenciaId = table.Column<int>(type: "int", nullable: false),
+                    TendenciaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chamados", x => x.ChamadoId);
+                    table.ForeignKey(
+                        name: "FK_Chamados_Gravidades_GravidadeId",
+                        column: x => x.GravidadeId,
+                        principalTable: "Gravidades",
+                        principalColumn: "GravidadeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Chamados_Modalidades_ModalidadeId",
+                        column: x => x.ModalidadeId,
+                        principalTable: "Modalidades",
+                        principalColumn: "ModalidadeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Chamados_Status_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "Status",
+                        principalColumn: "StatusId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Chamados_Tendencias_TendenciaId",
+                        column: x => x.TendenciaId,
+                        principalTable: "Tendencias",
+                        principalColumn: "TendenciaId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Chamados_Urgencias_UrgenciaId",
+                        column: x => x.UrgenciaId,
+                        principalTable: "Urgencias",
+                        principalColumn: "UrgenciaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enderecos",
+                columns: table => new
+                {
+                    EnderecoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Logradouro = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cep = table.Column<decimal>(type: "decimal(8,0)", nullable: false),
+                    Bairro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cidade = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UF = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: false),
+                    FilialId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enderecos", x => x.EnderecoId);
+                    table.ForeignKey(
+                        name: "FK_Enderecos_Filiais_FilialId",
+                        column: x => x.FilialId,
+                        principalTable: "Filiais",
+                        principalColumn: "FilialId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equipamentos",
+                columns: table => new
+                {
+                    EquipamentoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Marca = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Modelo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumeroSerie = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Potencia = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImagemUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FilialId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipamentos", x => x.EquipamentoId);
+                    table.ForeignKey(
+                        name: "FK_Equipamentos_Filiais_FilialId",
+                        column: x => x.FilialId,
+                        principalTable: "Filiais",
+                        principalColumn: "FilialId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemChamados",
+                columns: table => new
+                {
+                    ItemChamadoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChamadoId = table.Column<int>(type: "int", nullable: false),
+                    EquipamentoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemChamados", x => x.ItemChamadoId);
+                    table.ForeignKey(
+                        name: "FK_ItemChamados_Chamados_ChamadoId",
+                        column: x => x.ChamadoId,
+                        principalTable: "Chamados",
+                        principalColumn: "ChamadoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemChamados_Equipamentos_EquipamentoId",
+                        column: x => x.EquipamentoId,
+                        principalTable: "Equipamentos",
+                        principalColumn: "EquipamentoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chamados_GravidadeId",
+                table: "Chamados",
+                column: "GravidadeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chamados_ModalidadeId",
+                table: "Chamados",
+                column: "ModalidadeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chamados_StatusId",
+                table: "Chamados",
+                column: "StatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chamados_TendenciaId",
+                table: "Chamados",
+                column: "TendenciaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chamados_UrgenciaId",
+                table: "Chamados",
+                column: "UrgenciaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enderecos_FilialId",
+                table: "Enderecos",
+                column: "FilialId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equipamentos_FilialId",
+                table: "Equipamentos",
+                column: "FilialId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Filiais_ClienteId",
+                table: "Filiais",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemChamados_ChamadoId",
+                table: "ItemChamados",
+                column: "ChamadoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemChamados_EquipamentoId",
+                table: "ItemChamados",
+                column: "EquipamentoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Enderecos");
+
+            migrationBuilder.DropTable(
+                name: "ItemChamados");
+
+            migrationBuilder.DropTable(
+                name: "Chamados");
+
+            migrationBuilder.DropTable(
+                name: "Equipamentos");
+
+            migrationBuilder.DropTable(
+                name: "Gravidades");
+
+            migrationBuilder.DropTable(
+                name: "Modalidades");
+
+            migrationBuilder.DropTable(
+                name: "Status");
+
+            migrationBuilder.DropTable(
+                name: "Tendencias");
+
+            migrationBuilder.DropTable(
+                name: "Urgencias");
+
+            migrationBuilder.DropTable(
+                name: "Filiais");
+
+            migrationBuilder.DropTable(
+                name: "Lanches");
+        }
+    }
+}

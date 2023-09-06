24e55c00a84ece12ab3d913fe12ab18f13397430
DanilloAlvesSouza <danillo.alves.souza@outlook.com>
2023-09-06 01:21:46 -0300

    Populando tabelas da matriz GUT

---
+++ SiteDeVendas/Migrations/20230905231904_PopularGravidades.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KontrolaPoc.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularGravidades : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Gravidades(Descricao)" +
+                "VALUES('Sem gravidade')");
+            migrationBuilder.Sql("INSERT INTO Gravidades(Descricao)" +
+                "VALUES('Pouco grave')");
+            migrationBuilder.Sql("INSERT INTO Gravidades(Descricao)" +
+                "VALUES('Grave')");
+            migrationBuilder.Sql("INSERT INTO Gravidades(Descricao)" +
+                "VALUES('Muito Grave')");
+            migrationBuilder.Sql("INSERT INTO Gravidades(Descricao)" +
+                "VALUES('Extremamente grave')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Gravidades");
+        }
+    }
+}
---
+++ SiteDeVendas/Migrations/20230906041348_PopularTabelasGut.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KontrolaPoc.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularTabelasGut : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Urgencias(Descricao)" +
+                "VALUES('Pode esperar')");
+            migrationBuilder.Sql("INSERT INTO Urgencias(Descricao)" +
+                "VALUES('Pouco urgente')");
+            migrationBuilder.Sql("INSERT INTO Urgencias(Descricao)" +
+                "VALUES('Urgente, merece atenção no curto prazo')");
+            migrationBuilder.Sql("INSERT INTO Urgencias(Descricao)" +
+                "VALUES('Muito urgente')");
+            migrationBuilder.Sql("INSERT INTO Urgencias(Descricao)" +
+                "VALUES('Necessidade de ação imediata')");
+            migrationBuilder.Sql("INSERT INTO Tendencias(Descricao)" +
+                "VALUES('Não irá mudar')");
+            migrationBuilder.Sql("INSERT INTO Tendencias(Descricao)" +
+                "VALUES('Irá piorar a longo prazo')");
+            migrationBuilder.Sql("INSERT INTO Tendencias(Descricao)" +
+                "VALUES('Irá piorar a médio prazo')");
+            migrationBuilder.Sql("INSERT INTO Tendencias(Descricao)" +
+                "VALUES('Irá piorar a curto prazo')");
+            migrationBuilder.Sql("INSERT INTO Tendencias(Descricao)" +
+                "VALUES('Irá piorar rapidamente')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Urgencias");
+            migrationBuilder.Sql("DELETE FROM Tendencias");
+        }
+    }
+}
